04850b261ddb13776de800c4684e03802579d916
UEHARA Junji <uehaj@jggug.org>
2009-11-15 17:20:52 +0900

    add Makefile for groovy client

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+
+bin/groovyclient: src/main/c/groovyclient.c
+	gcc -g -o bin/groovyclient src/main/c/groovyclient.c
--- src/test/groovy/org/jggug/kobo/groovyserv/ExecTest.groovy
+++ src/test/groovy/org/jggug/kobo/groovyserv/ExecTest.groovy
@@ -4,12 +4,15 @@ import groovy.util.GroovyTestCase
 
 /**
  * Tests for the {@link org.jggug.kobo.groovyserv.Dump} class.
+ * Before running this, you must start groovyserver.
  */
 class ExecTest extends GroovyTestCase {
     static final String NL = System.getProperty("line.separator");
+    static final String FS = System.getProperty("file.separator");
 
     void testExec() {
-        Process p = "bin\\groovyclient.exe -e 'println \"hello\" '".execute()
+//        Process p = "bin\\groovyclient.exe -e 'println \"hello\" '".execute()
+        Process p = "bin"+FS+"groovyclient.exe -e 'println \"hello\" '".execute()
         println "----------------"
         p.getInputStream().eachLine {
             assert it == "hello"
