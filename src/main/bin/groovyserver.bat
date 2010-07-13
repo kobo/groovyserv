c8057740ad77123504c134e63819cd9fa2696e77
Y.Nakano <ynak@jggug.org>
2010-07-13 14:35:02 +0900

    made groovyserver.bat be able to run on cygwin console

--- src/main/bin/groovyserver.bat
+++ src/main/bin/groovyserver.bat
@@ -1,42 +1,72 @@
 @echo off
-@rem Copyright 2009-2010 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem     http://www.apache.org/licenses/LICENSE-2.0
-@rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
-@rem
+rem -----------------------------------------------------------------------
+rem Copyright 2009-2010 the original author or authors.
+rem
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+rem -----------------------------------------------------------------------
 
 setlocal
 
-@rem Determine what directory it is in.
+rem -----------------------------------
+rem Resolve base directory
+rem -----------------------------------
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.\
-
 set GROOVYSERV_HOME=%DIRNAME%..
+rem echo DEBUG: GROOVYSERV_HOME: %GROOVYSERV_HOME%
 
+rem -----------------
+rem Parse parameters
+rem -----------------
 :loop
-if "%1"=="" goto break
-if "%1"=="-q" set GROOVYSERV_OPTS=%GROOVYSERV_OPTS% -Dgroovyserver.verbose=false
-if "%1"=="-v" set GROOVYSERV_OPTS=%GROOVYSERV_OPTS% -Dgroovyserver.verbose=true
-if NOT "%1"=="-p" goto no_p
-  shift
-  set GROOVYSERV_OPTS=%GROOVYSERV_OPTS% -Dgroovyserver.port=%1
-:no_p
+if "%1" == "" (
+    goto break
+) else if "%1" == "-v" (
+    set GROOVYSERV_OPTS=%GROOVYSERV_OPTS% -Dgroovyserver.verbose=true
+) else if "%1" == "-p" (
+    if "%2" == "" (
+        echo ERROR: port number must be specified
+        exit 1
+    )
+    set GROOVYSERV_OPTS=%GROOVYSERV_OPTS% -Dgroovyserver.port=%2
+)
 shift
 goto loop
 :break
+rem echo DEBUG: GROOVYSERV_OPTS: %GROOVYSERV_OPTS%
 
+rem ---------------
+rem Set class path
+rem ---------------
 set CP=%GROOVYSERV_HOME%\lib\jna-3.2.2.jar;
 set CP=%GROOVYSERV_HOME%\lib\groovyserv-${project.version}.jar;%CP%
 set CLASSPATH=%CP%
+rem echo DEBUG: CLASSPATH: %CLASSPATH%
 
+rem -------------------------
+rem Compatibility for cygwin
+rem -------------------------
+if "%JAVA_HOME:~0,9%" == "/cygdrive" (
+    for /f %%z in ('cygpath.exe -d "%JAVA_HOME%"') do set JAVA_HOME=%%z
+)
+if "%GROOVY_HOME:~0,9%" == "/cygdrive" (
+    for /f %%z in ('cygpath.exe -d "%GROOVY_HOME%"') do set GROOVY_HOME=%%z
+)
+rem echo DEBUG: JAVA_HOME: %JAVA_HOME%
+rem echo DEBUG: GROOVY_HOME: %GROOVY_HOME%
+
+rem -------------
+rem Start server
+rem -------------
 start "groovyserver" /B %GROOVY_HOME%\bin\groovy %GROOVYSERV_OPTS% -e "org.jggug.kobo.groovyserv.GroovyServer.main(args)"
 
