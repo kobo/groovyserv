b53222a05f8de8bb8802ae4a1276c53581f5baff
UEHARA Junji <uehaj@jggug.org>
2010-12-17 16:18:54 +0900

    fix /* comment to /** comment.

--- src/test/groovy/org/jggug/kobo/groovyserv/TestUtils.groovy
+++ src/test/groovy/org/jggug/kobo/groovyserv/TestUtils.groovy
@@ -55,7 +55,7 @@ class TestUtils {
         return clientExecs + args
     }
 
-    /*
+    /**
      * Get a groovyclient command line which can invoke a groovycient instance with specified
      * environment variable(s).
      */
