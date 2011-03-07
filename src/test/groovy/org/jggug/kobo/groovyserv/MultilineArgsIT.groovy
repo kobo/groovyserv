6e8306688fa9934ec5f2b22e95517ae0d8afc17c
Yasuharu NAKANO <ynak@jggug.org>
2011-03-07 19:53:30 +0900

    added a test case for multiline arguments

---
+++ src/test/groovy/org/jggug/kobo/groovyserv/MultilineArgsIT.groovy
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jggug.kobo.groovyserv
+
+import groovy.util.GroovyTestCase
+
+/**
+ * Tests for the {@code groovyclient}.
+ * Before running this, you must start groovyserver.
+ */
+class MultilineArgsIT extends GroovyTestCase {
+
+    static final String SEP = System.getProperty("line.separator")
+
+    void testMultilineArgs() {
+        def script = """\
+            |println("start")
+            |println '''line1
+            |line2
+            |line3'''
+            |println("end")
+            |""".stripMargin()
+        assertEquals(
+            """start
+              |line1
+              |line2
+              |line3
+              |end
+              |""".stripMargin(), 
+            TestUtils.executeClientOk(["-e", "$script"]).text
+        )
+    }
+
+}
