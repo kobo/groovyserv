46d6469df902260c3631411093ec5dc9405f2b77
Y.Nakano <ynak@jggug.org>
2011-03-23 17:38:52 +0900

    fixed for a "glibc detected" error

--- src/main/c/session.c
+++ src/main/c/session.c
@@ -179,17 +179,18 @@ void send_header(int fd, int argc, char** argv, char* cookie)
     buf_printf(&read_buf, "%s: %s\n", HEADER_KEY_COOKIE, cookie);
 
     // send command line arguments.
-    char* encoded;
+    char *encoded_ptr, *encoded_work;
     for (i = 1; i < argc; i++) {
         if (argv[i] != NULL) {
-            encoded = malloc(sizeof(argv[i]) * 1.5); // base64 encoded data is less 1.5 times as much as raw data
-            if (encoded == NULL) {
+            encoded_ptr = malloc(strlen(argv[i]) * 1.5); // base64 encoded data is less 1.5 times as much as raw data
+            if (encoded_ptr == NULL) {
                 perror("ERROR: failed to malloc");
                 exit(1);
             }
-            base64_encode(encoded, (unsigned char*) argv[i]);
-            buf_printf(&read_buf, "%s: %s\n", HEADER_KEY_ARG, encoded);
-            free(encoded);
+            encoded_work = encoded_ptr; // copy for free
+            base64_encode(encoded_work, (unsigned char*) argv[i]);
+            buf_printf(&read_buf, "%s: %s\n", HEADER_KEY_ARG, encoded_work);
+            free(encoded_ptr);
         }
     }
 
