8fddf1ed76dcca7b11a038fbf42075aebd3c45d5
Yasuharu NAKANO <ynak@jggug.org>
2011-07-01 18:34:50 +0900

    fixes for a type check of ThreadDeath exception

--- src/main/groovy/org/jggug/kobo/groovyserv/GServThreadGroup.groovy
+++ src/main/groovy/org/jggug/kobo/groovyserv/GServThreadGroup.groovy
@@ -31,10 +31,22 @@ class GServThreadGroup extends ThreadGroup {
 
     @Override
     void uncaughtException(Thread thread, Throwable e) {
-        if (e in ThreadDeath) {
+        if (containsCaused(e, ThreadDeath)) {
             DebugUtils.verboseLog("${name}: Thread is stopped by force: ${thread}") // ignored details
         } else {
             DebugUtils.errorLog("${name}: Uncaught exception: ${thread}", e)
+        }
+    }
+
+    private boolean containsCaused(Throwable e, Class exceptionType) {
+        if (e == null) {
+            return false
+        }
+        if (e in exceptionType) {
+            return true
+        }
+        if (e.cause) {
+            return containsCaused(e.cause, exceptionType)
         }
     }
 
